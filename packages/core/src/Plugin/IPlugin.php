d8088422727931805ae219242b9689ef37d51a6c
0zd0 <67220210+0zd0@users.noreply.github.com>
2025-10-13 01:58:36 +0300

    feat(core): loader

--- packages/core/src/Plugin/IPlugin.php
+++ packages/core/src/Plugin/IPlugin.php
@@ -4,16 +4,16 @@ declare(strict_types=1);
 namespace LunaPress\CoreContracts\Plugin;
 
 use LunaPress\FoundationContracts\Container\IContainerBuilder;
+use LunaPress\FoundationContracts\Module\HasModules;
+use LunaPress\FoundationContracts\Package\HasPackages;
 use LunaPress\FoundationContracts\Support\Activatable;
 use LunaPress\FoundationContracts\Support\Deactivatable;
 use LunaPress\FoundationContracts\Support\Bootable;
 use LunaPress\FoundationContracts\Support\HasDi;
-use LunaPress\FoundationContracts\Module\MutableModules;
-use LunaPress\FoundationContracts\Package\MutablePackages;
 
 defined('ABSPATH') || exit;
 
-interface IPlugin extends Activatable, Deactivatable, MutableModules, MutablePackages, Bootable, HasDi
+interface IPlugin extends Activatable, Deactivatable, Bootable, HasDi, HasModules, HasPackages
 {
     public function setContainerBuilder(IContainerBuilder $builder): self;
     public function getPrefix(): string;
