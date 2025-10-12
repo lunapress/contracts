4ecb7302d4a423bba178d8937d268f57ca34243f
0zd0 <67220210+0zd0@users.noreply.github.com>
2025-10-12 04:48:14 +0300

    feat(foundation): remove wp-relatedness

--- packages/core/src/Subscriber/ISubscriberRegistry.php
+++ packages/core/src/Subscriber/ISubscriberRegistry.php
@@ -1,7 +1,9 @@
 <?php
 declare(strict_types=1);
 
-namespace LunaPress\FoundationContracts\Subscriber;
+namespace LunaPress\CoreContracts\Subscriber;
+
+use LunaPress\FoundationContracts\Subscriber\Subscriber;
 
 defined('ABSPATH') || exit;
 
