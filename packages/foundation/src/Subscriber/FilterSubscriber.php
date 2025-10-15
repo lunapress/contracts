40b6c2c9e6720097e15e371857939d80347769c5
0zd0 <67220210+0zd0@users.noreply.github.com>
2025-10-15 22:48:55 +0300

    feat(foundation): any subscribers in the shared layer

--- packages/foundation/src/Subscriber/FilterSubscriber.php
+++ packages/foundation/src/Subscriber/FilterSubscriber.php
@@ -1,9 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace LunaPress\CoreContracts\Subscriber;
-
-use LunaPress\FoundationContracts\Subscriber\Subscriber;
+namespace LunaPress\FoundationContracts\Subscriber;
 
 defined('ABSPATH') || exit;
 
