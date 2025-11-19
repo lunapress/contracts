e3b38bbc1e55161320d03784115dae77dca381cb
0zd0 <67220210+0zd0@users.noreply.github.com>
2025-11-20 00:51:49 +0300

    feat(wp-i18n): load_script_textdomain

--- packages/wp-i18n/src/LoadPluginTextDomain/ILoadPluginTextDomainFunction.php
+++ packages/wp-i18n/src/LoadPluginTextDomain/ILoadPluginTextDomainFunction.php
@@ -3,9 +3,11 @@ declare(strict_types=1);
 
 namespace LunaPress\Wp\I18nContracts\LoadPluginTextDomain;
 
+use LunaPress\CoreContracts\Support\IExecutableFunction;
+
 defined('ABSPATH') || exit;
 
-interface ILoadPluginTextDomainFunction
+interface ILoadPluginTextDomainFunction extends IExecutableFunction
 {
     public function domain(string $domain): self;
     public function pluginRelPath(string|false $pluginRelPath): self;
