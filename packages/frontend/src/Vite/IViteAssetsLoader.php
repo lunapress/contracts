19578fb3deee9a49cb1f82fd511c8a1a2a83651a
0zd0 <67220210+0zd0@users.noreply.github.com>
2026-04-23 23:36:11 +0400

    fix(frontend): correct dependencies for the loader

--- packages/frontend/src/Vite/IViteAssetsLoader.php
+++ packages/frontend/src/Vite/IViteAssetsLoader.php
@@ -4,15 +4,14 @@ declare(strict_types=1);
 
 namespace LunaPress\FrontendContracts\Vite;
 
-use LunaPress\Wp\AssetsContracts\Entity\IAssetDependency;
-use LunaPress\Wp\AssetsContracts\Entity\IWpEnqueueScriptModuleDep;
-
+use BackedEnum;
+use LunaPress\Wp\AssetsContracts\DTO\ScriptModuleDependency;
 
 interface IViteAssetsLoader
 {
     /**
      * @param IViteEntryPoint[] $entryPoints
-     * @param array<IWpEnqueueScriptModuleDep|IAssetDependency> $dependencies
+     * @param array<ScriptModuleDependency|string|BackedEnum> $dependencies
      */
     public function connect(array $entryPoints, bool $isAdmin = false, array $dependencies = []): void;
 }
